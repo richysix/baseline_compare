de5c7dcad5e366e459fa732b187027baac8cab35
Richard White <richard.white@sanger.ac.uk>
2018-11-13 20:51:31 +0000

    Add reactive object exptData to server.R

--- server.R
+++ server.R
@@ -1,6 +1,9 @@
 library(shiny)
 library(SummarizedExperiment)
 
+# source functions
+source(file.path('R', 'load_data.R'))
+
 # load baseline data
 # loads object called baseline_data
 load(file.path('data', 'Mm_baseline_data.rda'))
@@ -12,6 +15,35 @@ server <- function(input, output, session) {
   session$userData[['testing']] <- TRUE
   
   # load samples and counts files
+  exptData <- reactive({
+    if (session$userData[['debug']]) {
+      print('Function: mergedCounts')
+    }
+    if (session$userData[['testing']]) {
+      sample_file <- file.path('data', 'Brd2-samples.txt')
+      count_file <- file.path('data', 'Brd2-counts.tsv')
+      expt_data <-
+        load_data(sample_file, count_file, Mm_baseline, session)
+    } else{
+      # sample_file_info <- input$sample_file
+      # count_file_info <- input$count_file
+      # if (!is.null(sampleFileInfo) &
+      #     !is.null(countFileInfo)) {
+      #   exptData <-
+      #     load_data(sampleFileInfo$datapath,
+      #               countFileInfo$datapath,
+      #               input$dataType,
+      #               session)
+      # } else if (!is.null(dataFileInfo)) {
+      #   load(dataFileInfo$datapath)
+      # }
+      # else{
+      #   return(NULL)
+      # }
+    }
+    return(expt_data)
+  })
+    
   # check columns
   
   # compare to baseline data
