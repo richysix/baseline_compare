3180c8baf0ca695cc641589ef48ad58571bcd009
Richard White <richard.white@sanger.ac.uk>
2018-11-14 10:25:37 +0000

    ui.R: Create basic tab panel layout
    Contains tabs for Files, PCA, Results, Count Plot and Help.
    PCA tab contains the initial PCA plot.

--- ui.R
+++ ui.R
@@ -2,5 +2,18 @@ library(shiny)
 
 # UI for application
 ui <- fluidPage(
-  
+  navbarPage(
+    "Baseline CompaRe",
+    tabPanel("Files"
+    ),
+    tabPanel("PCA",
+             plotOutput("pca_plot")
+    ),
+    tabPanel("Results"
+    ),
+    tabPanel("Count Plot"
+    ),
+    tabPanel("Help"
+    )
+  )
 )
