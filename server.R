5290126715f197c643c1b7aeba0e072ed680500e
Richard White <richard.white@sanger.ac.uk>
2018-11-14 10:27:51 +0000

    server.R: Create quick PCA plot
    Added code to create a basic PCA plot to test the PCA output tab.

--- server.R
+++ server.R
@@ -1,5 +1,7 @@
 library(shiny)
 library(SummarizedExperiment)
+library(DESeq2)
+library(ggplot2)
 
 # source functions
 source(file.path('R', 'load_data.R'))
@@ -15,14 +17,14 @@ server <- function(input, output, session) {
   session$userData[['testing']] <- TRUE
   
   # load samples and counts files
-  exptData <- reactive({
+  combinedData <- reactive({
     if (session$userData[['debug']]) {
       print('Function: mergedCounts')
     }
     if (session$userData[['testing']]) {
       sample_file <- file.path('data', 'Brd2-samples.txt')
       count_file <- file.path('data', 'Brd2-counts.tsv')
-      expt_data <-
+      combined_data <-
         load_data(sample_file, count_file, Mm_baseline, session)
     } else{
       # sample_file_info <- input$sample_file
@@ -41,17 +43,58 @@ server <- function(input, output, session) {
       #   return(NULL)
       # }
     }
-    return(expt_data)
+    return(combined_data)
   })
-    
+  
   # check columns
   
   # compare to baseline data
   # check sample stages and gene ids
   
   # combine data, getting appropriate baseline samples by stage
+  dds_plus_baseline <- reactive({
+    combined_data <- combinedData()
+    dds <- DESeqDataSet(combined_data, design = ~ sex + condition)
+    return(dds)
+  })
   
   # run PCA
+  pca_plot_obj <- reactive({
+    dds <- dds_plus_baseline()
+    if (session$userData[['debug']]) {
+      print('Function: pca_plot_obj')
+      print('Variance Stabilizing Transform begin...')
+    }
+    
+    dds_vst <- varianceStabilizingTransformation(dds, blind=TRUE)
+    
+    if (session$userData[['debug']]) {
+      cat('Variance Stabilizing Transform done.\n')
+    }
+    
+    pca <- prcomp( t( assay(dds_vst) ) )
+    propVarPC <- pca$sdev^2 / sum( pca$sdev^2 )
+    aload <- abs(pca$rotation)
+    propVarRegion <- sweep(aload, 2, colSums(aload), "/")
+    plot_data <- data.frame(
+      pc1 = pca$x[,1],
+      pc2 = pca$x[,2],
+      shape = factor(c(rep('het', 6), rep('hom', 5), rep('wt', 3), rep('baseline', 111)),
+                     levels = c('baseline', 'wt', 'het', 'hom')),
+      colour = colData(dds_vst)[['stage']]
+    )
+    pca_plot <- ggplot(data = plot_data) + 
+      geom_point( aes(x = pc1, y = pc2, shape = shape, fill = colour)) + 
+      scale_shape_manual(values = c(21:24)) + 
+      guides(fill = guide_legend(override.aes = list(shape = 21)))
+    
+    return(pca_plot)
+  })
+  
+  # render heatmap plot
+  output$pca_plot <- renderPlot({
+    return(pca_plot_obj())
+  })
   
   # run DESeq2
   
