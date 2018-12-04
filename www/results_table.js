c2af9489cd170af81f0ac50f5a76c49f2fbc914a
Richard White <richard.white@sanger.ac.uk>
2018-12-04 12:51:37 +0000

    Tidy up

--- www/results_table.js
+++ www/results_table.js
@@ -1,19 +1,6 @@
 var debug = true
 var testing = true
 
-//function setParameters(user_data) {
-//    if (debug) {
-//        console.log(debug, testing);
-//        console.log(user_data);
-//    }
-//    debug = user_data.debug;
-//    testing = user_data.testing;
-//    
-//    if (debug) {
-//        console.log(debug, testing);
-//    }
-//}
-
 function addResultsLinks(msg) {
     var links = document.getElementsByClassName("results_button");
     if (debug) {
@@ -37,7 +24,4 @@ function addResultsLinks(msg) {
 $(document).ready( function() {
     addResultsLinks('initialise')
     
-    //Shiny.addCustomMessageHandler('set_up', setParameters)
-    //
-    //Shiny.addCustomMessageHandler('counts_link', addCountPlotLinks)
 });
